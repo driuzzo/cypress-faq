72fae99f9f4d55148c5e8c43880398a27aaa4203
adriano <adriano.driuzzo@hotmail.com>
2024-06-06 09:02:35 -0300

    alteracao da estrutura para receber mais exemplos

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://staging.example.com', // url padrão, por exemplo: staging
+    baseUrl: 'https://www.asus.com/content/asus-software/', // url padrão, por exemplo: staging
     experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
